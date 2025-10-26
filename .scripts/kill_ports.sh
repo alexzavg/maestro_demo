261dc06e702630fc1f54e5482e53fe0c6560ca35
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2025-10-26 15:36:53 +0100

    updates to maestro setup

--- .scripts/kill_ports.sh
+++ .scripts/kill_ports.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Array of ports to free up
-PORTS=(5037 5554 5555 5556 5557 5558 5559 5560 5561 5584 5585)  # ADB and emulator ports
+PORTS=(5037 5554 5555 5556 5557 5558 5559 5560 5561 5584 5585 9999)  # ADB and emulator ports
 
 for PORT in "${PORTS[@]}"; do
   PIDS=$(lsof -t -i:$PORT)
