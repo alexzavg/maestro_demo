a3409dda313effb0645148ca5d76ae8c6fe7a33e
Alexander Zavgorodnii <oleksandrzavhorodnii@coherentsolutions.com>
2025-11-26 02:52:38 +0100

    upd

--- .maestro/src/pages/MainScreen.page.js
+++
@@ -1,3 +0,0 @@
-output.mainScreen = {
-    headerImg: "org.wikipedia:id/view_announcement_header_image",
-};
---
+++ .maestro/src/pages/SignUp.page.js
@@ -0,0 +1,4 @@
+output.signUp = {
+    skipBtn: "org.wikipedia:id/fragment_onboarding_skip_button",
+    onboardingText: ".*The Free Encyclopedia.*",
+};
